cf3a63d5a6667a2f68af4e00392ad50f7e81a22b
zalviandyr <zukronalviandy@gmail.com>
2020-08-03 09:19:00 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="public/assets/css/font-awesome.css">
+    <title>Zera Shortlink</title>
+</head>
+
+<body>
+    <div class="card mt-5 mx-auto" style="width: 18rem;">
+        <div class="card-body text-center">
+            <h5 class="card-title">Social Media</h5>
+            <a href="https://bit.ly/30kTxpT" class="btn btn-danger btn-block"><i
+                    class="fa fa-youtube fa-fw mr-2"></i>Youtube</a>
+            <a href="https://bit.ly/2PjYLvL" class="btn btn-secondary mt-2 btn-block"><i
+                    class="fa fa-github fa-fw mr-2"></i>Github</a>
+            <a href="https://bit.ly/33huwOf" class="btn btn-info mt-2 btn-block"><i
+                    class="fa fa-facebook fa-fw mr-2"></i>Facebook</a>
+            <a href="https://bit.ly/2PnGmOp" class="btn btn-warning mt-2 btn-block"><i
+                    class="fa fa-instagram fa-fw mr-2"></i>Instagram</a>
+        </div>
+    </div>
+
+    <script src="public/assets/js/jquery-3.4.1.slim.min.js"></script>
+    <script src="public/assets/js/popper.min.js"></script>
+    <script src="public/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
